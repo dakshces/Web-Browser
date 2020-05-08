31cb0dd0783f747ca6c34fdd276e38defabcf178
Davin Lin <lindavin@grinnell.edu>
2020-05-08 12:50:05 -0400

    Add notes and comments

--- src/renderingengine/CSSParser.java
+++ src/renderingengine/CSSParser.java
@@ -67,18 +67,29 @@ public class CSSParser
 		return res;
 	}
 
+	/**
+	 * Changes currpos to the index of the next non-whitespace character.
+	 * currpos is unchanged if input.charAt(currpos) is not a whitespace character.
+	 */
 	public void consumeWhiteSpace()
 	{
 		consumeWhile(c -> Character.isWhitespace(c));
-	}
+	} // consumeWhiteSpace()
 
 
+	/**
+	 * Parses a CSS rule
+	 * @return
+	 */
 	public Rule parseRule()
 	{
 		return new Rule(parseSelectors(), parseDeclarations());
-	}
+	} // parseRule()
 
-
+	/**
+	 * Parses the CSS selectors
+	 * @return
+	 */
 	public ArrayList<Selector> parseSelectors()
 	{
 		char next = input.charAt(currPos);
@@ -104,8 +115,13 @@ public class CSSParser
 		Collections.sort(selectors); //Sort by specificity in descending order
 		System.out.println("Selectors: " + selectors);
 		return selectors;
-	}
+	} // parseSelectors()
 
+	/**
+	 * Parses simple CSS selectors. One type of selectors
+	 * that we neglect for compounded selectors
+	 * @return
+	 */
 	public SimpleSelector parseSimpleSelector()
 	{
 		char next = input.charAt(currPos);
@@ -139,8 +155,12 @@ public class CSSParser
 		}
 
 		return selector;
-	}
+	} // parseSimpleSelectors()
 
+	/**
+	 * Parses the entire set of declarations
+	 * @return
+	 */
 	public ArrayList<Declaration> parseDeclarations()
 	{
 		ArrayList<Declaration> declarations = new ArrayList<Declaration>(); 
@@ -154,8 +174,13 @@ public class CSSParser
 		}
 		currPos++; //Skip }
 		return declarations;
-	}
+	} // parseDeclarations()
 
+	/**
+	 * Parses a single declaration which has the structure 
+	 * identifier=value
+	 * @return
+	 */
 	public Declaration parseDeclaration()
 	{
 		Declaration dec = new Declaration();
@@ -168,12 +193,16 @@ public class CSSParser
 		currPos++; //Skip ; (end of declaration)
 
 		return dec;
-	}
+	} // parseDeclaration()
 
+	/**
+	 * Parses the identifier component of the declaration
+	 * @return
+	 */
 	public String parseIdentifier()
 	{
 		return consumeWhile(c -> validIdentifierChar(c));
-	}
+	} // parseIdentifier()
 
 
 	public boolean validIdentifierChar(char c)
@@ -181,7 +210,11 @@ public class CSSParser
 		return (c == '-' || c == '_' || Character.isLetterOrDigit(c));
 	}
 
-
+	/**
+	 * Parses the value component of the declaration. Supports two types of
+	 * declaration identifiers currently : color and padding
+	 * @return
+	 */
 	public Value parseValue()
 	{
 		char next = input.charAt(currPos);
@@ -191,7 +224,7 @@ public class CSSParser
 			return parseColor();
 
 		return new Keyword(parseIdentifier());
-	}
+	} // parseValue()
 
 	public Length parseLength()
 	{
@@ -250,3 +283,23 @@ public class CSSParser
 
 } // Class CSSParser
 
+/*
+ * Notes:
+ * If the identifier requires there to be a unit and no unit is
+ * provided then the identifier is declaration is ignored.
+ * For the example of padding, it requires the value to be numbers separated
+ * by spaces with a max of four numbers and minimum of one number, each number
+ * must have a unit unless the number is zero.
+ * padding dictates a different effect on the element's box depending on the 
+ * value recieved (i.e. if the value is one, two, three or four numbers.
+ * 
+ * The identifier has preconditions for the given value that needs to be met,
+ * before it can be used. Maybe we can use Predicates to dictate the preconditions?
+ * 
+ * Then we need to consider that how we construct the StyleNode out of the element
+ * and the Style. Rules contains two vectors : selectors and declarations. parseSelectors
+ * seem to be implemented fine. parseDeclarations needs more stuff (nuance?). 
+ * 
+ * Sooooo.... we only add a declaration into ArrayList<Declaration> declarations
+ * only when the preconditions for the specific name is met.
+ */
